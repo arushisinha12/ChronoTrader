59dd73ddf54ebf3e6496eb07d0700cb327c37e4f
Arushi Sinha <arushisinha025@gmail.com>
2025-11-20 17:57:54 +0530

    FINAL

--- game_app/views.py
+++ game_app/views.py
@@ -364,6 +364,7 @@ def reset_game(request):
         "Your objective: acquire {FUEL_GOAL} Gold Coins (Temporal Fuel). "
         "Use Jumps to exploit shifting markets. Traversal costs {TIME_JUMP_COST} credits. "
         "Remember: Items purchased locally are sold at a steep loss in the same era. You must travel to profit!"
+        "Click on instructions for more details."
     ).format(FUEL_GOAL=FUEL_GOAL, TIME_JUMP_COST=TIME_JUMP_COST)
 
     messages.info(request, full_message) 
